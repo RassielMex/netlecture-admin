d1fa44bf78a1c2d031c4f8dca8a330b677a1ae8f
RassielMex <rhcp94.rm@gmail.com>
2024-01-22 21:03:57 -0600

    deleted back button

--- src/components/MainAppBar.tsx
+++ src/components/MainAppBar.tsx
@@ -1,45 +1,31 @@
-import { useLocation, useNavigate } from "react-router-dom";
 import FilterMenu from "./FilterMenu";
 import SearchBar from "./SearchBar";
 import { useAppDispatch } from "../hooks/hooks";
 import { logOut } from "../store/slices/login-slice";
 import { Button, Navbar } from "flowbite-react";
 import { MouseEventHandler } from "react";
-import {
-  ArrowLeftOnRectangleIcon,
-  ArrowUturnLeftIcon,
-} from "@heroicons/react/24/solid";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
 type Props = {};
 
 const MainAppBar = (props: Props) => {
-  const location = useLocation();
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
   const handleLogOut: MouseEventHandler = () => {
     dispatch(logOut());
   };
-  const handleReturn: MouseEventHandler = () => {
-    navigate("/", { replace: true });
-  };
+
   return (
     <Navbar fluid className="bg-blue-600 mb-4">
       <div className="flex justify-end w-full">
         <Navbar.Toggle />
         <Navbar.Collapse>
-          {location.pathname === "/" ? (
-            <>
-              <SearchBar />
-              <FilterMenu />
-              <Button color="blue" onClick={handleLogOut}>
-                <ArrowLeftOnRectangleIcon className="h-5 w-5" />
-              </Button>
-            </>
-          ) : (
-            <Button color="blue" onClick={handleReturn}>
-              <ArrowUturnLeftIcon className="h-5 w-5" />
+          <>
+            <SearchBar />
+            <FilterMenu />
+            <Button color="blue" onClick={handleLogOut}>
+              <ArrowLeftOnRectangleIcon className="h-5 w-5" />
             </Button>
-          )}
+          </>
         </Navbar.Collapse>
       </div>
     </Navbar>
