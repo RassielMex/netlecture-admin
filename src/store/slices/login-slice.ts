2427972cd250952437cab8f81eba9b99850bc5e7
RassielMex <rhcp94.rm@gmail.com>
2024-01-22 21:01:46 -0600

    access and refresh

--- src/store/slices/login-slice.ts
+++ src/store/slices/login-slice.ts
@@ -2,20 +2,24 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "../../models/User";
 
 import type { AppDispatch } from "../store";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 // Define a type for the slice state
 interface LoginState {
   isLoggedIn: boolean;
   error: boolean;
   message: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
 // Define the initial state using that type
 const initialState: LoginState = {
-  isLoggedIn: true,
+  isLoggedIn: false,
   error: false,
   message: "",
+  accessToken: "",
+  refreshToken: "",
 };
 
 export const loginSlice = createSlice({
@@ -23,10 +27,15 @@ export const loginSlice = createSlice({
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    logSuccess: (state) => {
+    logSuccess: (
+      state,
+      action: PayloadAction<{ accessToken: string; refreshToken: string }>
+    ) => {
       state.isLoggedIn = true;
       state.error = false;
       state.message = "";
+      state.accessToken = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
     },
     logError: (state, action: PayloadAction<string>) => {
       state.error = true;
@@ -44,11 +53,22 @@ export const loginSlice = createSlice({
 export const onLogin = (user: User) => {
   return async (dispatch: AppDispatch) => {
     try {
-      const request = await axios.post("", user);
+      const request = await axios.post(
+        "https://netlecture-api.vercel.app/user/token/",
+        user
+      );
       if ((request.status = 200)) {
-        dispatch(logSuccess());
+        dispatch(
+          logSuccess({
+            accessToken: request.data?.access,
+            refreshToken: request.data?.refresh,
+          })
+        );
       }
     } catch (error) {
+      if (error instanceof AxiosError) {
+        console.log(error.response?.data?.detail);
+      }
       dispatch(logError("Error al intentar acceder"));
     }
   };
