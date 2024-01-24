d7ec018fcb6c679dd00d4912d0ac6e645c55e486
RassielMex <rhcp94.rm@gmail.com>
2024-01-23 19:01:38 -0600

    modal for loading

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import { Modal, Spinner } from "flowbite-react";
+import { useAppSelector } from "../hooks/hooks";
 
 type Props = {};
 
 const Login = (props: Props) => {
+  const loading = useAppSelector((state) => {
+    return state.login.loading;
+  });
   return (
     <div className="w-screen h-screen bg-login-gradient bg-no-repeat bg-cover">
       <div className="flex flex-col justify-center items-center h-full">
         <LoginForm />
+        <Modal show={loading} size={"sm"}>
+          <Modal.Body>
+            <Spinner />
+            <span className="ml-2">Ingresando...</span>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
